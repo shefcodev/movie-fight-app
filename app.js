92f123157a4ef7ec4408a7589d26cc026b5694bb
shefcodev <sherefjimoh@gmail.com>
2023-06-06 23:12:58 +0100

    get movies data from omdbapi.com

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 // project javascript file;
 
-console.log("Hello World!");
+const fetchData = async (query) => {
+    const response = await axios.get("http://www.omdbapi.com/", {
+        params: {
+            apikey: "2269a1a6",
+            s: query,
+        }
+    });
+    console.log(response.data);
+}
+
+const input = document.getElementById("input");
+
+let timeoutId;
+const onInput = event => {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        fetchData(event.target.value.trim());
+    }, 1000)
+}
+
+input.addEventListener("input", onInput) 
