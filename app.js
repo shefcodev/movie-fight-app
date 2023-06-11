eea7639ef81f5f0b9f802550cda8c9dcd0961845
shefcodev <sherefjimoh@gmail.com>
2023-06-11 20:08:17 +0100

    made a follow-up request to get data for selected movie

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ const onInput = async event => {
         movieOption.addEventListener("click", () => {
             dropdown.classList.remove("is-active"); 
             input.value = movie.Title;
+            onMovieSelect(movie);
         })
         resultsWrapper.appendChild(movieOption);
     });
@@ -64,3 +65,13 @@ document.addEventListener("click", event => {
         dropdown.classList.remove("is-active");
     }
 })
+
+const onMovieSelect = async (movie) => {
+    const response = await axios.get("http://www.omdbapi.com/", {
+        params: {
+            apikey: "2269a1a6",
+            i: movie.imdbID,
+        }
+    })
+    console.log(response.data);
+}
